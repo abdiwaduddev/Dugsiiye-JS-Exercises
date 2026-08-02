2ea00f75dad131702b82c197ce5657319597ccde
abdiwaduddev <abdibasidhussein38@gmail.com>
2026-08-02 21:51:35 +0300

    Submitted Exercise 1

---
+++ Exercise 1/main.js
@@ -0,0 +1,12 @@
+x = 7;
+y = 3;
+
+console.log(x + y);
+console.log(x - y);
+console.log(x * y);
+console.log(x / y);
+console.log(x % y);
+console.log(x++);
+console.log(++y);
+console.log(--x);
+console.log(y--);
