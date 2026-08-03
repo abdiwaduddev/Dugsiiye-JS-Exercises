6144dac07d544caafb8d494df3e7c9712aeca0a1
abdiwaduddev <abdibasidhussein38@gmail.com>
2026-08-03 20:59:15 +0300

    Submitted Exercise 4

---
+++ Exercise 4/main.js
@@ -0,0 +1,23 @@
+// function declation
+
+function add(a, b) {
+  return a + b;
+}
+
+console.log(add(10, 40));
+console.log(add(1, 4));
+console.log(add(13, 20));
+console.log(add(16, 34));
+console.log(add(30, 90));
+
+// function expression
+
+const addition = function (a, b) {
+  return a + b;
+};
+
+console.log(addition(10, 40));
+console.log(addition(1, 4));
+console.log(addition(13, 20));
+console.log(addition(16, 34));
+console.log(addition(30, 90));
