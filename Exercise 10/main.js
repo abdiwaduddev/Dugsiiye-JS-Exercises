e8fb85bc5753ebe22a1fc104edba7cc57c6d2a67
abdiwaduddev <abdibasidhussein38@gmail.com>
2026-08-05 22:13:41 +0300

    Submitted Exercise 10

---
+++ Exercise 10/main.js
@@ -0,0 +1,7 @@
+let Library = [
+  { name: "Rich Dad Poor Dad", Author: "Robert", Year: 1990 },
+  { name: "Atomic Habits", Author: "Jamis Clear", Year: 2010 },
+];
+
+console.log(Library[0].name);
+console.log(Library[1].Author);
