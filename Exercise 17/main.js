b34bd740b4bd47f85a706e2c81c08621ac198a69
abdiwaduddev <abdibasidhussein38@gmail.com>
2026-08-09 22:23:59 +0300

    Submitted Exercise 17 else if

---
+++ Exercise 17/main.js
@@ -0,0 +1,11 @@
+let temp = -10;
+
+if ((temp < 0)) {
+  console.log("Very Cold");
+} else if (temp <= 15){
+  console.log("Cold");
+} else if (temp <= 25) {
+  console.log("Warm");
+} else {
+  console.log("Hot");
+}
