79cf15a03ec996cacfa4268b1ed2eb099b12615e
abdiwaduddev <abdibasidhussein38@gmail.com>
2026-08-18 22:10:35 +0300

    Exercise 33 for Changing Content

---
+++ Exercise 33/main.js
@@ -0,0 +1,7 @@
+const header = document.querySelector('#header')
+const content = document.querySelector('.content')
+
+function changeContent(){
+    header.textContent = 'Please visit my website'
+    content.textContent = 'Thanks for visiting my website'
+}
