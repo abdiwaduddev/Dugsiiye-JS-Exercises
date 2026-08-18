d5d0d52c31d449da45272c4eabf087df2e102421
abdiwaduddev <abdibasidhussein38@gmail.com>
2026-08-18 21:48:08 +0300

    Submitted Exercise 32 Query Selector

---
+++ Exercise 32/main.js
@@ -0,0 +1,8 @@
+const head = document.querySelector("#head")
+const text = document.querySelectorAll(".text")[1]
+const more = document.querySelector("button")
+
+
+console.log(head)
+console.log(text)
+console.log(more)
